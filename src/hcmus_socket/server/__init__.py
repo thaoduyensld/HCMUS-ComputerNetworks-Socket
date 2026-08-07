2d35e8b994dbd9956320bffcc253ba00fe84016f
lamngoc0312 <nhungoccl55@gmail.com>
2026-08-07 08:51:39 +0700

    feat(server): add thread-safe session registry

--- src/hcmus_socket/server/__init__.py
+++ src/hcmus_socket/server/__init__.py
@@ -7,11 +7,14 @@ from .download import (
     handle_download_frame,
 )
 from .logger import ServerLogger
+from .registry import ActiveSessionRegistry, SessionSnapshot
 
 __all__ = [
     "DownloadTransferResult",
     "ServerLogger",
     "ServerPeer",
+    "ActiveSessionRegistry",
+    "SessionSnapshot",
     "handle_download",
     "handle_download_frame",
 ]
